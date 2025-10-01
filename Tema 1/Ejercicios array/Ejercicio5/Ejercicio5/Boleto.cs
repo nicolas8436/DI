ae4364131d206fccba95741b10c37336cc6e9ff7
nicolas8436 <nicolashijon@gmail.com>
2025-10-01 10:24:18 +0200

    01/10/25-V4

---
+++ Tema 1/Ejercicios array/Ejercicio5/Ejercicio5/Boleto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    public class Boleto
+    {
+        private int[] Numboleto;
+
+        private int numeroAleatorio()
+        {
+            return new Random().Next(1, 50);
+        }
+        public Boleto()
+        {
+            Numboleto = new int[6];
+        }
+
+        public int ObtenerPosicion(int indice)
+        {
+            return Numboleto[indice];
+        }
+
+    }
+}
+
+//Mirar si se puede concatenar para hacer las comprobaciones
+
+//No puede haber dos numeros iguales en el mismo boleto
+//No puede habeer un boleto con los 6 mismos numeros es decir 1 al 6 y lg otro q cambie minimo un numero 
---
+++ Tema 1/Ejercicios array/Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] ListaBoletos = new int[100][];
+
+        }
+    }
+}
